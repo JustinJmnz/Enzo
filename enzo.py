53cd615c7e5351383c2d0ef21ad1314e0eac9e4d
JustinJmnz <justinjmnz@gmail.com>
2016-11-17 23:38:43 +0000

    Adding API key

--- enzo.py
+++ enzo.py
@@ -1,16 +1,37 @@
 import speech_recognition as sr
 from gtts import gTTS
 import os
-  
+import sys
+
+# Repeats a given string to the user through usb speaker
 def repeater(string_to_repeat):
-	print("Repeating: " + string_to_repeat)
 	string_to_repeat = string_to_repeat + "."
 	tts = gTTS(text=(string_to_repeat),lang="en")
 	tts.save("output.mp3")
-	##os.system("aplay -f dat output.mp3")
 	os.system("mplayer output.mp3")
-  
+
+# Splits string into words
+# if word[0] == call -> call
+# if word[0] == text -> getMessage() -> text
+
+def parseAudio(audio):
+	splitAudio = audio.split()
+	print("splitAudio[0]: " + splitAudio[0])
+	print("splitAudio[1]: " + splitAudio[1])
+	if(splitAudio[0] == "call"):
+		return "Calling: " + splitAudio[1]
+	elif(splitAudio[0] == "text"):
+		repeater("What is your message?")
+		message = listener()
+		return "Texting: " + splitAudio[1] + " Message: " + message
+	else:
+		return audio
+
+# Listens for speech
+# Calls Google API to detect words
+# Returns Google's Interpretation as String
 def listener():
+	 # Listens for a voice
      r = sr.Recognizer()
      with sr.Microphone(sample_rate = 48000) as source:
          audio = r.adjust_for_ambient_noise(source)
@@ -20,11 +41,19 @@ def listener():
          try:
               print("processing")
               what_i_said = r.recognize_google(audio)
-              print("Google Speech Recognition " + what_i_said)
-              repeater(what_i_said)
+              return what_i_said
          except sr.UnknownValueError:
               print("Google Speech Recognition could not understand your audio")
+              return None
          except sr.RequestError as e:
               print("Could not request results from Google Speech Recognition service; {0}".format(e))
+              return None
   
-listener()
+# Start of program
+if __name__ == "main":
+	what_i_said = listener()
+	print(what_i_said)
+	if(what_i_said == None):
+		sys.exit()
+	command = parseAudio(what_i_said)
+	print(command)
